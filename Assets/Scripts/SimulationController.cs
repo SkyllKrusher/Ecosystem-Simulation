79973bdb6cb563fd31304e672d443edd75e06df4
AyushBharwdaj <bhardwaj.ayush007@gmail.com>
2020-07-26 17:42:38 +0530

    Simulation speed control added

---
+++ Assets/Scripts/SimulationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationController : MonoBehaviour
+{
+    [Range(0, 10)] [SerializeField] private float simulationSpeed;
+
+    private void Start()
+    {
+        UpdateSimulationSpeed();
+    }
+
+    private void OnValidate()
+    {
+        UpdateSimulationSpeed();
+    }
+
+    private void UpdateSimulationSpeed()
+    {
+        Time.timeScale = simulationSpeed;
+    }
+}
