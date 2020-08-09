f66453aa9435b88d78da6f3a93baf17c84c95c7b
AyushBharwdaj <bhardwaj.ayush007@gmail.com>
2020-08-09 22:45:02 +0530

    UI follow target (for stats)

---
+++ Assets/Scripts/UIFollowTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollowTarget : MonoBehaviour
+{
+    [SerializeField] private Vector3 Offset;
+    [SerializeField] private Transform followTarget;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        // transform.LookAt(cam.transform.position);
+        transform.position = cam.WorldToScreenPoint(followTarget.position + Offset);
+    }
+}
